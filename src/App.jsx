6ef231ea0b75514dde233114eb2a597e85eb5048
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 09:44:04 -0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react"
 import Cardsidebar from "./assets/componentes/Cardsidebar"
 import Container from "./assets/componentes/Container"
 import ConteudoPrincipal from "./assets/componentes/ConteudoPrincipal"
 import Header from "./assets/componentes/Header"
 import Sidebar from "./assets/componentes/Sidebar"
+import { useEffect } from "react"
 
 function App() {
+
+const [artistas, setArtistas] = useState ([])
+const [isLoading, setIsLoading] = useState(false)
+
+useEffect(() => {
+  fetch('http://localhost:3000/artistas')
+  .then(res => res.json())
+  .then(res => setArtistas(res))
+  .catch(err => console.log(err))
+
+}, [])
 
   return (
     <>
@@ -15,7 +28,19 @@ function App() {
         <Cardsidebar/>
         <Cardsidebar/>
         </Sidebar>
-      <ConteudoPrincipal/>
+        <ConteudoPrincipal>
+          {artistas.map ((artista) => (
+              <div className="bg-red 500 w-28 h-28 flex flex-col justify-around items-center">
+                <p>{artista.name}</p>
+                <div className="bg-green-400 w-3/4 h-7"></div>
+              </div>  
+            ))}
+          
+        <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+        </ConteudoPrincipal>
       </Container>
     </>
   )
